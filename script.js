2326b771b75bede33a5c30c2d3a80f731751d937
Mohammed-Abdulrahman517 <moodyxdream@gmail.com>
2026-06-02 08:19:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll",()=>{
+
+const navbar=document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+
+navbar.style.background="#000";
+
+}
+else{
+
+navbar.style.background="rgba(0,0,0,.7)";
+
+}
+
+});
